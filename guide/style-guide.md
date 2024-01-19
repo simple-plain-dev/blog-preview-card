713a405b5d91ab6d1bf8eb326340a76a5b902107
safrilth <safriltaufiqhidayat@gmail.com>
2024-01-19 21:04:18 +0700

    final

---
+++ guide/style-guide.md
@@ -0,0 +1,35 @@
+# Front-end Style Guide
+
+## Layout
+
+The designs were created to the following widths:
+
+- Mobile: 375px
+- Desktop: 1440px
+
+## Colors
+
+### Primary
+
+--clr-primary-1:hsl(47, 88%, 63%);
+--clr-neutral-1: hsl(0, 0%, 100%);
+--clr-neutral-2: hsl(0, 0%, 50%);
+--clr-neutral-3: hsl(0, 0%, 7%);
+--fs: 16px;
+--ff: "Figtree";
+--fw-medium: 600;
+--fw-bold: 800;
+
+### Neutral
+
+
+## Typography
+
+### Body Copy
+
+- Font size (paragraph): 16px
+
+### Font
+
+- Family: [Figtree](https://fonts.google.com/specimen/Figtree)
+- Weights: 600, 800
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,87 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import banner from '$lib/assets/images/illustration-article.svg'
+  import avatar from '$lib/assets/images/image-avatar.webp'
+</script>
+
+<main>
+  <img class="banner" src={banner} alt="banner" >
+  <header>
+    <span class="tag bold">Learning</span>
+    <p class="small">Publised 21 Dec 2023</p>
+  </header>
+  <section class="description">
+    <h3>HTML & CSS foundations</h3>
+    <p class="gray">These languages are the backbone of every website, defining structure, content, and presentation</p>
+  </section>
+    <footer>
+    <img src={avatar} alt="avatar" class="avatar">
+    <p class="bold">Greg Hooper</p>
+  </footer>
+</main>
+
+<style>
+  main {
+    background-color: var(--clr-neutral-1);
+    border-radius: 1rem;
+    margin-inline: 1.5rem;
+    padding: 1.5rem;
+    box-shadow: 0.5rem 0.5rem 0 0 black;
+    max-width: 25rem;
+  }
+
+  header {
+    display: grid;
+    gap: 1rem;
+    margin-block: 1rem;
+  }
+
+  .banner {
+    width: 100%;
+    object-fit: cover;
+    border-radius: 0.5rem;
+  }
+
+  .tag {
+    background-color: var(--clr-primary-1);
+    padding-block: 0.5rem;
+    padding-inline: 1rem;
+    border-radius: 0.5rem;
+    margin-right: auto;
+  }
+
+  .description {
+    display: grid;
+    gap: 1rem;
+  }
+
+  footer {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    gap: 1rem;
+    align-items: center;
+    margin-top: 1rem;
+  }
+
+  .avatar {
+    width: 2rem;
+  }
+
+  /* typography */
+
+  p {
+    line-height: 1.5rem;
+  }
+
+  .bold {
+    font-weight: var(--fw-bold);
+  }
+
+  .small {
+    font-size: small;
+  }
+
+  .gray {
+    color: var(--clr-neutral-2);
+  }
+
+</style>
